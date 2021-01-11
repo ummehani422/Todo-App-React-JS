3c919a4b06be5ff1f425d5ba174cd312f84cce51
ummehani422 <ahmedhania266@gmail.com>
2021-01-11 04:05:14 -0800

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [],
+      value : ""
+    }
+  }
+  Add_todo=()=>{
+    this.state.todos.push(this.state.value)
+    this.setState({
+      todos : this.state.todos,
+      value: ''
+    })
+  }
+  Delete_todo=(index)=>{
+   this.state.todos.splice(index,1);
+   this.setState({
+     todos: this.state.todos
+   })
+  }
+  Edit_todo=(index)=>{
+var updated_value = prompt("Enter updated value")
+this.state.todos[index]= updated_value
+this.setState({
+  todos: this.state.todos
+}) 
+  }
+  render() {
+    let {todos,value} = this.state;
+    return (
+      <div>
+        {/* <Header/> */}
+        <input value={value} onChange={(e)=>this.setState({value: e.target.value})} type="text" placeholder="Enter Item" />
+        <button onClick={this.Add_todo}>Add Item</button>
+       
+        <ul>{todos.map((v, i) => {
+          return <li key={i}>{v}
+            <button onClick={()=>this.Edit_todo(i)}>Edit Item</button>
+           <button onClick={()=>this.Delete_todo(i)}>Delete Item</button>
+          </li>
+          
+        })}</ul>
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+// import React, { Component } from 'react';
+
+// class Header extends Component{
+//     render(){
+//         return(
+//             <div>
+//      <navbar expand="lg" variant="light" bg="light">
+//         <container>
+//           <navbar.brand href="#">Navbar</navbar.brand>
+//         </container>
+//       </navbar>
+//             </div>
+//         )
+//     }
+// }
+// export default Header;
